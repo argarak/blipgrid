f8aa84c5334ac5fc1b9d54ce1dd6a554ecc93baf
argarak <dev@argarak.me>
2023-09-27 15:21:54 +0100

    add key handler to handle key presses

---
+++ keys.js
@@ -0,0 +1,60 @@
+
+class KeyHandler {
+    constructor() {
+        this.registeredKeys = [];
+        this.currentlyPressed = [];
+
+        let self = this;
+
+        document.addEventListener("keydown", e => {
+            self.handleKeyDown(e);
+        });
+
+        document.addEventListener("keydown", e => {
+            self.handleKeyUp(e);
+        });
+    }
+
+    handleKeyUp(e) {
+        // remove the keys from currentlyPressed
+        this.currentlyPressed = this.currentlyPressed.filter(kp => kp == e.key);
+    }
+
+    handleKeyDown(e) {
+        if (this.currentlyPressed.includes(e.key)) return;
+
+        // add the key into currentlyPressed
+        this.currentlyPressed.push(e.key);
+
+        for (let registeredKey of this.registeredKeys) {
+            if (registeredKey.keys.length !==
+                this.currentlyPressed.length) continue;
+
+            let keyIndex = 0;
+            while (registeredKey.keys[keyIndex].toLowerCase() ===
+                   this.currentlyPressed[keyIndex].toLowerCase()) {
+                keyIndex++;
+
+                if (keyIndex === registeredKey.keys.length) {
+                    e.preventDefault();
+                    registeredKey.fn();
+                    this.currentlyPressed = [];
+                    break;
+                }
+            }
+        }
+    }
+
+    registerKey(keys, fn) {
+        this.registeredKeys.push({
+            "keys": keys,
+            "fn": fn
+        });
+    }
+}
+
+let keyHandler = new KeyHandler();
+keyHandler.registerKey(["Shift", "p"], () => {console.log("hello!");});
+keyHandler.registerKey(["control", "x"], () => {console.log("cut!");});
+keyHandler.registerKey(["control", "c"], () => {console.log("copy!");});
+keyHandler.registerKey(["control", "p"], () => {console.log("paste");});
