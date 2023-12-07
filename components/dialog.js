326df310383de151bdbb617932722f41fcc119be
argarak <dev@argarak.me>
2023-12-07 17:22:41 +0000

    add dialog

---
+++ components/dialog.js
@@ -0,0 +1,46 @@
+import * as mdiStyle from "@material-design-icons/font/index.css?inline";
+import { LitElement, html, css, unsafeCSS } from "lit";
+import { ref, createRef } from "lit/directives/ref.js";
+import * as dialogStyle from "/styles/components/dialog.styl?inline";
+
+class Dialog extends LitElement {
+    static properties = {
+        title: { type: String }
+    };
+
+    _onCloseClick(e) {
+        this.close();
+    }
+
+    close() {
+        this.remove();
+    }
+
+    render() {
+        return html`
+        <div id="dialog">
+          <div id="header">
+            <button @click=${this._onCloseClick}>
+              <span class="material-icons">close</span>
+            </button>
+            ${this.title}
+          </div>
+          <div id="body"><slot></slot></div>
+        </div>`;
+    }
+
+    static styles = [
+        css`${unsafeCSS(mdiStyle.default)}`,
+        css`${unsafeCSS(dialogStyle.default)}`
+    ];
+
+    constructor() {
+        super();
+
+        this.title = "dialog";
+    }
+
+}
+
+customElements.define("ui-dialog", Dialog);
+export default Dialog;
