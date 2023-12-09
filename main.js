dcae1d2104320710985e742d21b448fe9763d3d0
argarak <dev@argarak.me>
2023-12-09 13:57:17 +0000

    fix static references to node_modules

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 import "/styles/main.styl";
 
+import "@ibm/plex/css/ibm-plex.min.css";
+import "@material-design-icons/font";
+
+import "/util.js";
+import "/components/knob.js";
+import "/components/dialog.js";
+import "/components/welcome-dialog.js";
+import "/components/edit-patch.js";
+import "/components/edit-view.js";
+import "/components/trigger-grid.js";
+import "/components/arpeggiator.js";
+import "/components/sequencer.js";
+
 import * as Tone from "tone";
+
 import Patch from "./patch.js";
 import Mixer from "./mixer.js";
-
 import keyHandler from "./keys.js";
 
 import * as basicPatch from "./objects/patches/basic.json";
 import * as basicSynthPatch from "./objects/patches/basic-synth.json";
-
-// TODO: note release
 
 document.addEventListener("DOMContentLoaded", () => {
     let btnPlay = document.getElementById("btnPlay");
